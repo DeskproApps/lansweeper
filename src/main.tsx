fd9784410cc29a4c253a5706126392ce1fa9d368
ilia makarov <ilia.makarov@me.com>
2024-04-24 17:10:28 +0300

    Rollback iframe-resizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
+import "iframe-resizer/js/iframeResizer.contentWindow.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render((
